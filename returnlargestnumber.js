7905fb0ff56ddc903879016a261d2459d84b68ac
Will Brown <wtbrown10@gmail.com>
2020-07-11 19:42:48 -0400

    return the largest number edit

--- returnlargestnumber.js
+++ returnlargestnumber.js
@@ -3,13 +3,14 @@ let array = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857,
 function largestOfFour(arr) {
 
     let newArray = []
+    let numberCheck = 0
 
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
+        for (let j = 0; j < 1; j++) {
             newArray.push(Math.max(...arr[i]))
         }
     }
-    console.log(Math.max(...newArray))
+    console.log(newArray)
     // return arr;
 }
 
