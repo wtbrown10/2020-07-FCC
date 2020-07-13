349a106fffb9647db3f6630729d0952528abdbb2
Will Brown <wtbrown10@gmail.com>
2020-07-13 15:57:25 -0400

    completed

--- finderskeepers.js
+++ finderskeepers.js
@@ -1,6 +1,16 @@
 function findElement(arr, func) {
     let num = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            num = arr[i];
+            i = arr.length
+        } else if (i === arr.length - 1 && num === 0) {
+            num = undefined
+        }
+    }
+
     return num;
 }
 
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+console.log(findElement([1, 5, 3, 7, 9, 11], num => num % 2 === 0));
