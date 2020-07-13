0b45ae76913f3651dc258745b695ec6619f29c36
Will Brown <wtbrown10@gmail.com>
2020-07-13 15:33:49 -0400

    added <=

--- confirmending.js
+++ confirmending.js
@@ -1,8 +1,4 @@
 function confirmEnding(str, target) {
-    // let word = str.split('')
-
-
-    // let lastLetter = Boolean(word[word.length - 1] === target)
 
     lastLetter = str.endsWith(target)
 
--- truncateastring.js
+++ truncateastring.js
@@ -1,9 +1,14 @@
 function truncateString(str, num) {
     let end = '...'
+
     let truncate = str.slice(0, num)
 
-    return truncate + end
+    if (str.length <= num) {
+        return str
+    } else {
+
+        return truncate + end
+    }
 }
 
 console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
-
