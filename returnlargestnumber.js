81dba77a27c7fb2da7b2bb5fd4f76f99014a202d
Will Brown <wtbrown10@gmail.com>
2020-07-13 22:43:36 -0400

    update more

--- returnlargestnumber.js
+++ returnlargestnumber.js
@@ -3,7 +3,6 @@ let array = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857,
 function largestOfFour(arr) {
 
     let newArray = []
-    let numberCheck = 0
 
     for (let i = 0; i < arr.length; i++) {
         newArray.push(Math.max(...arr[i]))
