27e9ac7f929539d58ebf1bd32ea93f0312a30e6f
Will Brown <wtbrown10@gmail.com>
2020-07-14 18:15:39 -0400

    mutation

---
+++ mutations.js
@@ -0,0 +1,12 @@
+function mutation(arr) {
+    var test = arr[1].toLowerCase();
+    var target = arr[0].toLowerCase();
+    for (var i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0)
+            return false;
+    }
+    return true;
+}
+
+console.log(mutation(["hello", "hey"]));
+
