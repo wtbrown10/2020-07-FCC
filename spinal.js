ded0c3f427e7aa3c16a0b11a72ee1b838e12c5ac
Will Brown <wtbrown10@gmail.com>
2020-07-20 19:37:01 -0400

    solved

--- spinal.js
+++ spinal.js
@@ -1,5 +1,13 @@
 function spinalCase(str) {
-    return str;
+    // Create a variable for the white space and underscores.
+    var regex = /\s+|_+/g;
+
+    // Replace low-upper case to low-space-uppercase
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+    // Replace space and underscore with -
+    return str.replace(regex, "-").toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+// test here
+spinalCase("This Is Spinal Tap");
