7781c5625160b52bcdf1e6eed2f4b33c91290d3b
Will Brown <wtbrown10@gmail.com>
2020-07-21 17:23:54 -0400

    solved

---
+++ missingletter.js
@@ -0,0 +1,19 @@
+function fearNotLetter(str) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let strSplit = str.split('')
+    let alphaSplit = alphabet.split('')
+    let firstLetter = alphabet.indexOf(str[0])
+    let lastLetter = str.length + 1
+    console.log(alphabet.indexOf(str[0]))
+
+    if (alphabet == str) {
+        return undefined
+    } else {
+        let word = alphaSplit.splice(firstLetter, lastLetter)
+        console.log(word)
+        let missingLetter = word.filter(letter => !strSplit.includes(letter)).join()
+
+        return missingLetter;
+    }
+}
+console.log(fearNotLetter("abce"));
